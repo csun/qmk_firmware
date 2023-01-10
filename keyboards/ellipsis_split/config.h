a94b21645654007559e1272605854607443a12fd
Cameron Sun <cameron.csun@gmail.com>
2023-01-10 01:37:46 -0800

    Attempt to get firmware running

---
+++ keyboards/ellipsis_split/config.h
@@ -0,0 +1,33 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+// Default debounce of 5 seems to be too little
+#define DEBOUNCE 5
+
+#define EE_HANDS
+#define MASTER_LEFT
+
+#define USE_I2C
+#define SOFT_SERIAL_PIN D2
+
+#define MATRIX_ROWS 10 // Double for split
+#define MATRIX_COLS 9
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 1
+#define BOOTMAGIC_LITE_ROW_RIGHT 6
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 0
+
+// Change this to how you wired your keyboard
+// COLS: Left to right, ROWS: Top to bottom
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5, F4, B5 } // Last pin unused, just need to match number of columns
+#define MATRIX_ROW_PINS_RIGHT { F4, F5, F6, F7, B1 }
+#define MATRIX_COL_PINS_RIGHT { D1, D0, D4, C6, D7, B4, E6, B6, B2 }
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION ROW2COL
+
+#endif
